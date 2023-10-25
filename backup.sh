ebf6d700871aa4a1c51a3f12baadb14b19f69f7e
priyankapatil250696 <142022631+priyankapatil250696@users.noreply.github.com>
2023-10-25 22:36:15 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "Logging in with personal access token."
+export GH_TOKEN=$BACKUP_GITHUB_PAT
+gh auth setup-git
+
+echo "Downloading repositories for" $BACKUP_GITHUB_OWNER
+gh repo list $BACKUP_BITHUB_OWNER --json "name" --limit 4 --template '{{range .}}{{ .name }}{{"\n"}}{{end}}' | xargs -L1 -I {} gh repo clone $BACKUP_GITHUB_OWNER/{}
+
+echo "downloaded repositories...."
+find . -maxdepth 1 -type d 
+
+echo "Uploading to S3 bucket" $BACKUP_BUCKET_NAME "in region" $BACKUP_AWS_REGION
